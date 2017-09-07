dab45abe8578f9032e4fdd456572443c2625c95a
Daniel <daniel51_1994@hotmail.com>
2017-09-07 16:21:56 -0700

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # comp4711-lab
 COMP4711 lab 1
+
+Daniel
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+    echo "Hello world!!!";
+    ?>
+    </body>
+</html>
